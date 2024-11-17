5c0294aba2d7ac9cf1a801a16f5acd48c7329928
Simon Yen <simonyen980821@gmail.com>
2024-11-18 00:05:58 +0800

    更改端口，准备开始部署

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ async fn main() {
 
     let router = router::get_router();
     let service=Service::new(router).hoop(Logger::new());
-    let acceptor = TcpListener::new("localhost:9999").bind().await;
+    let acceptor = TcpListener::new("0.0.0.0:19999").bind().await;
 
     Server::new(acceptor).serve(service).await;
 }
